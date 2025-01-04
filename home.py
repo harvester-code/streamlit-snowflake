29b3a6fad0c65eef3eeb964c4d35969e7f61884b
YI <yibyungha@gmail.com>
2025-01-05 00:59:56 +0900

    -

--- home.py
+++ home.py
@@ -3,7 +3,7 @@ import pandas
 import plotly.express as px
 from snowflake.snowpark import Session
 
-st.write('aaa')
+st.write("aaa")
 
 # # Snowflake 연결 생성 (Native App 환경에서 자동으로 연결)
 # conn = st.connection("snowflake")
@@ -12,14 +12,14 @@ st.write('aaa')
 # df = conn.query("SELECT * FROM sim_df_dict LIMIT 1")
 # st.dataframe(df)
 
-# 현재 활성화된 세션 가져오기
-session = Session.get_active_session()
+# # 현재 활성화된 세션 가져오기
+# session = Session.get_active_session()
 
-# 데이터 쿼리
-df = session.sql("SELECT * FROM sim_df_dict LIMIT 1").to_pandas()
+# # 데이터 쿼리
+# df = session.sql("SELECT * FROM sim_df_dict LIMIT 1").to_pandas()
 
-# Streamlit 앱 구성
-st.title("Snowflake Data Analysis")
+# # Streamlit 앱 구성
+# st.title("Snowflake Data Analysis")
 
-# 데이터 미리보기
-st.dataframe(df)
+# # 데이터 미리보기
+# st.dataframe(df)
