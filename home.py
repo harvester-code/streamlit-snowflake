b29756bac7c14d055fe13b073ca5777ed003e7f6
YI <yibyungha@gmail.com>
2025-01-04 23:52:55 +0900

    -

--- home.py
+++ home.py
@@ -1,3 +1,9 @@
 import streamlit as st
+import duckdb
 
-st.write("Hello, world!")
+# DuckDB 연결 생성
+conn = duckdb.connect(database=':memory:')
+
+# 예시 쿼리 실행
+result = conn.execute("SELECT 'Hello from DuckDB' as message").fetchall()
+st.write(result)
