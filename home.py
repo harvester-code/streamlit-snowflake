2b3f7ca1a4a3863695307927f4a59eaaf7d1ac30
YI <yibyungha@gmail.com>
2025-01-05 01:47:25 +0900

    -

--- home.py
+++ home.py
@@ -3,20 +3,29 @@ import pandas as pd
 import plotly.express as px
 from snowflake.snowpark import Session
 from snowflake.snowpark.context import get_active_session
+import psycopg2
+from psycopg2.extras import RealDictCursor
+import json
 
 # 페이지 제목 설정
 st.title("Snowflake Data Analysis")
 
 session = get_active_session()
 
-# TPCH 샘플 데이터 조회
-st.subheader("TPCH Sample Data")
-tpch_sql = "SELECT * FROM snowflake_sample_data.tpch_sf1.lineitem LIMIT 20"
-tpch_data = session.sql(tpch_sql).collect()
-st.dataframe(tpch_data)
-
 # DEP_PAX 데이터 조회
 st.subheader("DEP_PAX Data")
 dep_pax_sql = 'SELECT * FROM dev_flexa_dwh_db.public."2402_DEP_PAX" LIMIT 20'
 dep_pax_data = session.sql(dep_pax_sql).collect()
 st.dataframe(dep_pax_data)
+
+secret_sql = "SELECT SECRET_STRING FROM SNOWFLAKE.ACCOUNT_USAGE.SECRETS WHERE SECRET_NAME = 'redshift_secret'"
+secret_result = session.sql(secret_sql).collect()
+redshift_config = json.loads(secret_result[0]["SECRET_STRING"])
+
+redshift_conn = psycopg2.connect(**redshift_config)
+cursor = redshift_conn.cursor(cursor_factory=RealDictCursor)
+
+table = "aircraft_all_history"
+sql = f"SELECT * FROM {table} LIMIT 10;"  # 처음 10개 행만 조회
+df = pd.read_sql(sql, redshift_conn)
+st.dataframe(df)
